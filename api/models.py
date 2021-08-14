bc4e809fd0d61972eac2213891e7fc209e2cc3b5
wagner obara <wagner.obara@atos.net>
2021-08-14 19:13:29 -0300

    api: Models + teste CRUD de clientes

--- api/models.py
+++ api/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    Modelo Produto
+    """
+    price = models.FloatField(null=False, blank=False, verbose_name='Preço', help_text='Preço do Produto')
+    image = models.URLField(max_length=200, blank=False, verbose_name='Imagem', help_text='URL da Imagem do Produto')
+    brand = models.CharField(max_length=100, null=False, blank=False, verbose_name='Marca', 
+                             help_text='Marca do Produto')
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name='Título', 
+                             help_text='Nome do Produto')
+    review_score = models.FloatField(null=True, blank=True, verbose_name='Média dos reviews', 
+                                     help_text='Média dos reviews para este Produto')
+
+    class Meta:
+        verbose_name = 'Produto'
+
+    def __str__(self):
+        return 'Produto {} da Marca {} com preço de R$ {}'.format(self.brand, self.title, self.price)
+
+    objects = models.Manager()
+
+
+class Customer(models.Model):
+    """
+    Modelo Cliente
+    """
+    # O cadastro dos clientes deve conter apenas seu nome e endereço de e-mail.
+    name = models.CharField(max_length=100, verbose_name='Nome')
+
+    # Marcando o campo como unique garante que um cliente se registrará duas vezes com o mesmo endereço de e-mail.
+    email = models.EmailField(max_length=50, verbose_name='E-Mail', unique=True, 
+                              error_messages={'unique': "Já existe um cliente cadastrado com o e-mail informado."})
+    
+    # Usar ManyToMany com Products garante que cada cliente só terá uma única lista de produtos, 
+    # e que a lista de produtos tenha uma quantidade ilimitada de produtos.
+    wish_list = models.ManyToManyField(Product, through='Wishlist')
+
+    class Meta:
+        verbose_name = 'Cliente'
+
+    def __str__(self):
+        return self.name
+
+    objects = models.Manager()
+
+
+class Wishlist(models.Model):
+    """
+    Modelo Lista produto favorito
+    """
+    customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
+
+    # Usar uma ForeignKey garante que apenas produtos existentes sejam adicionado na lista.
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        verbose_name = 'Lista de Produtos Favorito'
+
+        # Criar uma constraint de chave única para o cliente e produto, garante que um produto não esteja duplicado 
+        # na lista de produtos favoritos de um cliente.
+        unique_together = ['customer', 'product']
+
+    def __str__(self):
+        return 'Cliente {} possui em sua lista de favoritos o produto {}'.format(self.customer.name, self.product.title)
+
+    objects = models.Manager()
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,67 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.utils import IntegrityError
 from django.test import TestCase
 
-# Create your tests here.
+from api.models import Customer
+
+class CustomerTestCase(TestCase):
+    def setUp(self):
+        Customer.objects.create(name='Cliente 1', email='cliente1@luizalabs.com')
+        Customer.objects.create(name='Cliente 2', email='cliente2@luizalabs.com')
+        Customer.objects.create(name='Cliente 3', email='cliente3@luizalabs.com')
+
+    def test_customer_insert_count(self):
+        """Testa se os clientes foram inseridos corretamente."""
+        # Recupera todos os clientes
+        all_customers = Customer.objects.all()
+
+        # checa se há 3 clientes inseridos
+        self.assertTrue(len(all_customers) == 3)
+
+    def test_customer_insert(self):
+        """Testa se os clientes foram inseridos corretamente."""
+        customer_1 = Customer.objects.get(email='cliente1@luizalabs.com')
+        customer_2 = Customer.objects.get(email='cliente2@luizalabs.com')
+        customer_3 = Customer.objects.get(email='cliente3@luizalabs.com')
+
+        # checa se clientes foram inseridos:
+        self.assertEqual(customer_1.name, 'Cliente 1')
+        self.assertEqual(customer_1.email, 'cliente1@luizalabs.com')
+        self.assertEqual(customer_2.name, 'Cliente 2')
+        self.assertEqual(customer_2.email, 'cliente2@luizalabs.com')
+        self.assertEqual(customer_3.name, 'Cliente 3')
+        self.assertEqual(customer_3.email, 'cliente3@luizalabs.com')
+
+    def test_customer_insert_email_duplicated(self):
+        """Testa se o cliente não pode ser inserido duas vezes com o mesmo endereço de e-mail. """
+        with self.assertRaisesRegex(IntegrityError, 'duplicate key'):
+            Customer.objects.create(name='Cliente email duplicado', email='cliente1@luizalabs.com')
+
+    def test_customer_update(self):
+        """Testa se um cliente pode ser atualizado"""
+        # Recupera cliente para atualizar
+        customer_1 = Customer.objects.get(email='cliente1@luizalabs.com')
+        
+        # atualiza nome
+        customer_1.name = 'Cliente 1 Da silva'
+        customer_1.email = 'cliente1.da.silva@luizalabs.com'
+        customer_1.save()
+
+        # Recupera novamente
+        customer_1 = Customer.objects.get(email='cliente1.da.silva@luizalabs.com')
+
+        # checa se clientes foi atualizado corretamente
+        self.assertEqual(customer_1.name, 'Cliente 1 Da silva')
+        self.assertEqual(customer_1.email, 'cliente1.da.silva@luizalabs.com')
+    
+    def test_customer_delete(self):
+        """Testa se um cliente pode ser apagado"""
+        # Recupera cliente para deletar
+        customer_2 = Customer.objects.get(email='cliente2@luizalabs.com')
+
+        # deleta
+        customer_2.delete()
+
+        # Checa que cliente não existe mais
+        with self.assertRaises(ObjectDoesNotExist):
+            customer_2 = Customer.objects.get(email='cliente2@luizalabs.com')
