57a9f5fabc2706ed7ece995415a0c61be6a34586
wagner obara <wagner.obara@atos.net>
2021-08-14 19:56:24 -0300

    api: admin Models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Customer, Wishlist, Product
+
+
+class WishlistInline(admin.TabularInline):
+    model = Wishlist
+    fields = ['product']
+    extra = 0
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ('id', 'name', 'email')
+    fields = ['name', 'email', ]
+    search_fields = ['name', 'email']
+    inlines = [WishlistInline, ]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ('id', 'title', 'brand', 'price', 'image')
+    fields = ['title', 'brand', 'price', 'image', 'review_score']
+    list_filter = ('brand', )
+    search_fields = ['title', 'brand']
